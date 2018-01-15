b2ec75ad0b06ba9511553c7462b5736916a88c91
Sam <srdejong@uvic.ca>
2018-01-15 15:30:15 -0800

    Added draw.C script

---
+++ draw.C
@@ -0,0 +1,31 @@
+
+
+void draw(TString file, int ch, int event){
+
+  TFile *f = new TFile(file);
+  TTree *t = (TTree*)f->Get("wave");
+
+  stringstream ss;
+  ss<<ch;
+  TString c = ss.str();
+
+  vector<double> *data;
+  double xinc;
+  
+  t->SetBranchAddress("xinc", &xinc);
+  t->SetBranchAddress("ch"+c, &data);
+
+  t->GetEntry(event);
+
+  TGraph *gr = new TGraph();
+  
+  for(int i=0; i<data->size(); i++){
+    gr->SetPoint(i+1, i*xinc/1e-6, data->at(i));
+
+  }
+
+  gr->Draw("AL");
+
+
+
+}
